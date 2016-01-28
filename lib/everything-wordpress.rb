28b0fc5f129e77165eb89db554f0ddab648f9712
Kyle Tolle <kyle@nullsix.com>
2016-01-27 22:37:19 -0700

    Refactor to use everything gems code
    We can remove the Post-specific class here, since it’s been replaced by
    the Everything::Piece.
    We can use the #find_by_name_recursive to locate the piece as well.

--- lib/everything-wordpress.rb
+++ lib/everything-wordpress.rb
@@ -1,122 +1,25 @@
 require 'rubygems'
 require 'bundler/setup'
+Bundler.require(:default)
+require 'yaml'
 
-require 'dotenv'
 dotenv_path = File.join File.expand_path(File.dirname(__FILE__)), '../' , '.env'
 Dotenv.load dotenv_path
 
-require 'thor'
-require 'yaml'
-require 'rubypress'
-require 'fastenv'
 
 module Everything
-  class Post
-    class Directory
-      def initialize(post_dir_name)
-        @post_dir_name = post_dir_name
-      end
-
-      def full_path
-        glob_path      = File.join everything_path, '**', @post_dir_name
-        possible_dirs  = Dir.glob glob_path
-        full_post_path = possible_dirs.first
-
-        unless full_post_path
-          puts "Couldn't find a directory for the post #{@post_dir_name}."
-          return
-        end
-
-        unless File.directory? full_post_path
-          puts "Expected a directory but #{full_post_path} wasn't a directory."
-          return
-        end
-
-        full_post_path
-      end
-
-      private
-
-      def everything_path
-        Fastenv.everything_path
-      end
-    end
-
-    class Metadata
-      def initialize(full_path)
-        yaml_path = File.join full_path, 'index.yaml'
-        @metadata = YAML.load_file yaml_path
-      end
-
-      def [](value)
-        @metadata[value]
-      end
-    end
-
-    class Content
-      def initialize(full_path)
-        markdown_path  = File.join full_path, 'index.md'
-        @markdown_text = File.read markdown_path
-      end
-
-      def title
-        partitioned_text.first.sub('# ', '')
-      end
-
-      def body
-        partitioned_text.last
-      end
-
-      private
-
-      def partitioned_text
-        @partitioned_text ||= @markdown_text.partition("\n\n")
-      end
-    end
-
-    def initialize(post_dir_name)
-      @post_dir_name = post_dir_name
-      @directory     = Directory.new(@post_dir_name).full_path
-      @metadata      = Metadata.new(@directory)
-      @content       = Content.new(@directory)
-
-      unless public_post?
-        message = "Expected a public post, but #{post_dir_name}'s metadata didn't declare it to be public."
-        puts message
-        raise message
-      end
-    end
-
-    def name
-      File.join('', @post_dir_name)
-    end
-
-    def title
-      @content.title
-    end
-
-    def body
-      @content.body
-    end
-
-    def categories
-      @metadata['categories']
-    end
-
-    private
-
-    def public_post?
-      @metadata['public']
-    end
-  end
-
   class Wordpress < Thor
     desc "publish POST_DIR", "publish the blog in the directory POST_DIR to Wordpress"
     def publish(post_dir)
 
       wp = Rubypress::Client.new(host: Fastenv.wordpress_host, username: Fastenv.wordpress_username, password: Fastenv.wordpress_password)
 
-      post = Everything::Post.new(post_dir)
+      post = Everything::Piece.find_by_name_recursive(post_dir)
+      unless post.public?
+        message = "Expected a public post, but #{post_dir_name}'s metadata didn't declare it to be public."
+        puts message
+        raise message
+      end
 
       content =
         {
@@ -125,7 +28,7 @@ module Everything
           post_name:    post.name,
           post_content: post.body,
           post_author:  1,
-          terms_names:  { category: post.categories }
+          terms_names:  { category: post.metadata['categories'] }
       }
 
       everything_wordpress_path = Fastenv.everything_wordpress_path
