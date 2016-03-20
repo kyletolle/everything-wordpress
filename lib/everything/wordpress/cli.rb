f6877ddaf6a565e7017c994234a27371b4f7c7ad
Kyle Tolle <kyle@nullsix.com>
2016-03-20 13:18:53 -0600

    Use single quotes in wordpress cli

--- lib/everything/wordpress/cli.rb
+++ lib/everything/wordpress/cli.rb
@@ -3,12 +3,12 @@ require 'everything/wordpress'
 module Everything
   module Wordpress
     class CLI < Thor
-      desc "publish POST_DIR", "publish the blog in the directory POST_DIR to Wordpress"
+      desc 'publish POST_DIR', 'publish the blog in the directory POST_DIR to Wordpress'
       def publish(post_dir)
         Publisher.new(post_dir).publish
       end
 
-      desc "migrate_metadata", "migrate the legacy post metadata to the new post metadata"
+      desc 'migrate_metadata', 'migrate the legacy post metadata to the new post metadata'
       def migrate_metadata
         MetadataMigrator.new.migrate_to_new_metadata
       end
