518c9a4d480ab86e0f0c58dde5927118f15864ed
Kyle Tolle <kyle@nullsix.com>
2016-03-20 13:18:27 -0600

    Rename Publisher methods to follow ruby convention

--- lib/everything/wordpress/client.rb
+++ lib/everything/wordpress/client.rb
@@ -1,15 +1,21 @@
-require 'forwardable'
+module Everything
+  module Wordpress
+    class Client
+      def initialize
+        @wp ||= Rubypress::Client.new(
+            host:     Fastenv.wordpress_host,
+            username: Fastenv.wordpress_username,
+            password: Fastenv.wordpress_password
+          )
+      end
 
-class Client
-  extend Forwardable
+      def new_post(params)
+        @wp.newPost(params)
+      end
 
-  def initialize
-    @wp ||= Rubypress::Client.new(
-        host:     Fastenv.wordpress_host,
-        username: Fastenv.wordpress_username,
-        password: Fastenv.wordpress_password
-      )
+      def edit_post(params)
+        @wp.editPost(params)
+      end
+    end
   end
-
-  def_delegators :@wp, :newPost, :editPost
 end
--- lib/everything/wordpress/publisher.rb
+++ lib/everything/wordpress/publisher.rb
@@ -10,14 +10,14 @@ module Everything
 
       def publish
         if post.already_published?
-          client.editPost(post.publish_params)
+          client.edit_post(post.publish_params)
 
           post.update_metadata
 
           puts "Successfully updated #{post.name}"
 
         else
-          new_post_id = client.newPost(post.publish_params)
+          new_post_id = client.new_post(post.publish_params)
 
           post.update_metadata(new_post_id)
 
